308d57e8da7edb478cc9cffdb1654940003e60e5
Lihogub Oleg <oligli@yandex.ru>
2022-04-17 21:05:52 +0300

    Initial commit

---
+++ src/main/java/ru/lihogub/springoauth2webfluxkeycloak/SpringOauth2WebfluxKeycloakApplication.java
@@ -0,0 +1,13 @@
+package ru.lihogub.springoauth2webfluxkeycloak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringOauth2WebfluxKeycloakApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringOauth2WebfluxKeycloakApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/lihogub/springoauth2webfluxkeycloak/SpringOauth2WebfluxKeycloakApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.lihogub.springoauth2webfluxkeycloak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringOauth2WebfluxKeycloakApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
