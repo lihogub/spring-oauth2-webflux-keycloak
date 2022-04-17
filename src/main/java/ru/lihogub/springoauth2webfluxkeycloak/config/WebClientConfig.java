f9790722b034c73f030a5d551c6a4bc2a962c936
Lihogub Oleg <oligli@yandex.ru>
2022-04-18 00:04:28 +0300

    setup security and config

---
+++ src/main/java/ru/lihogub/springoauth2webfluxkeycloak/config/WebClientConfig.java
@@ -0,0 +1,29 @@
+package ru.lihogub.springoauth2webfluxkeycloak.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository;
+import org.springframework.security.oauth2.client.web.reactive.function.client.ServerOAuth2AuthorizedClientExchangeFilterFunction;
+import org.springframework.security.oauth2.client.web.server.ServerOAuth2AuthorizedClientRepository;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Configuration
+public class WebClientConfig {
+
+    @Bean
+    WebClient webClientDefault() {
+        return WebClient.create();
+    }
+
+    @Bean("oauth-client")
+    WebClient webClientOauthAuthorized(ReactiveClientRegistrationRepository clientRegistrations,
+                        ServerOAuth2AuthorizedClientRepository authorizedClients) {
+        ServerOAuth2AuthorizedClientExchangeFilterFunction oauth =
+                new ServerOAuth2AuthorizedClientExchangeFilterFunction(clientRegistrations, authorizedClients);
+        oauth.setDefaultClientRegistrationId("keycloak");
+        return WebClient.builder()
+                .filter(oauth)
+                .build();
+    }
+
+}
---
+++ src/main/java/ru/lihogub/springoauth2webfluxkeycloak/config/WebSecurityConfig.java
@@ -0,0 +1,30 @@
+package ru.lihogub.springoauth2webfluxkeycloak.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+@RequiredArgsConstructor
+public class WebSecurityConfig {
+
+    @Bean
+    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+        return http
+                .csrf().disable()
+                .authorizeExchange(exchanges -> exchanges
+                                .pathMatchers("/public").permitAll()
+                                .anyExchange().authenticated()
+                )
+                .oauth2ResourceServer(ServerHttpSecurity.OAuth2ResourceServerSpec::jwt)
+                .oauth2Login(Customizer.withDefaults())
+                .oauth2Client(Customizer.withDefaults())
+                .build();
+    }
+
+}
